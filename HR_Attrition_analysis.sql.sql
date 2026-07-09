152434b62aeb7e5a50cf0f16ce0e0f600c30e42e
Anshika Rawat <rawatanshikaa29@gmail.com>
2026-07-09 12:08:32 +0530

    Initial commit

---
+++ HR_Attrition_analysis.sql.sql
@@ -0,0 +1,107 @@
+CREATE DATABASE hr_project;
+USE hr_project;
+CREATE TABLE hr_employee_attrition (
+    Age INT,
+    Attrition VARCHAR(5),
+    BusinessTravel VARCHAR(30),
+    DailyRate INT,
+    Department VARCHAR(50),
+    DistanceFromHome INT,
+    Education INT,
+    EducationField VARCHAR(50),
+    EmployeeCount INT,
+    EmployeeNumber INT PRIMARY KEY,
+    EnvironmentSatisfaction INT,
+    Gender VARCHAR(10),
+    HourlyRate INT,
+    JobInvolvement INT,
+    JobLevel INT,
+    JobRole VARCHAR(50),
+    JobSatisfaction INT,
+    MaritalStatus VARCHAR(20),
+    MonthlyIncome INT,
+    MonthlyRate INT,
+    NumCompaniesWorked INT,
+    Over18 VARCHAR(5),
+    OverTime VARCHAR(5),
+    PercentSalaryHike INT,
+    PerformanceRating INT,
+    RelationshipSatisfaction INT,
+    StandardHours INT,
+    StockOptionLevel INT,
+    TotalWorkingYears INT,
+    TrainingTimesLastYear INT,
+    WorkLifeBalance INT,
+    YearsAtCompany INT,
+    YearsInCurrentRole INT,
+    YearsSinceLastPromotion INT,
+    YearsWithCurrManager INT
+);
+SELECT COUNT(*) FROM hr_employee_attrition;
+SELECT 
+    COUNT(*) AS Total_Employees,
+    SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) AS Attrition_Count,
+    ROUND(SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS Attrition_Rate_Percent
+FROM hr_employee_attrition;
+
+SELECT 
+    ROUND(AVG(Age), 0) AS Average_Age,
+    ROUND(AVG(MonthlyIncome), 0) AS Average_Monthly_Income
+FROM hr_employee_attrition
+
+SELECT 
+     Department,
+    COUNT(*) AS Total_Employees,
+    SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) AS Attrition_Count,
+    ROUND(SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS Attrition_Rate_Percent
+FROM hr_employee_attrition
+GROUP BY Department
+ORDER BY Attrition_Rate_Percent DESC;
+
+SELECT 
+    Gender,
+    COUNT(*) AS Total_Employees,
+    SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) AS Attrition_Count,
+    ROUND(SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS Attrition_Rate_Percent
+FROM hr_employee_attrition
+GROUP BY Gender;
+
+SELECT 
+    JobRole,
+    COUNT(*) AS Total_Employees,
+    SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) AS Attrition_Count,
+    ROUND(SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS Attrition_Rate_Percent
+FROM hr_employee_attrition
+GROUP BY JobRole
+ORDER BY Attrition_Rate_Percent DESC;
+
+SELECT 
+    OverTime,
+    COUNT(*) AS Total_Employees,
+    SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) AS Attrition_Count,
+    ROUND(SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS Attrition_Rate_Percent
+FROM hr_employee_attrition
+GROUP BY OverTime;
+
+SELECT 
+    Attrition,
+    ROUND(AVG(MonthlyIncome), 0) AS Average_Monthly_Income,
+    ROUND(AVG(TotalWorkingYears), 1) AS Average_Working_Years,
+    ROUND(AVG(YearsAtCompany), 1) AS Average_Years_At_Company
+FROM hr_employee_attrition
+GROUP BY Attrition;
+
+SELECT 
+    CASE 
+        WHEN Age < 25 THEN '18-24'
+        WHEN Age BETWEEN 25 AND 34 THEN '25-34'
+        WHEN Age BETWEEN 35 AND 44 THEN '35-44'
+        WHEN Age BETWEEN 45 AND 54 THEN '45-54'
+        ELSE '55+'
+    END AS Age_Group,
+    COUNT(*) AS Total_Employees,
+    SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) AS Attrition_Count,
+    ROUND(SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS Attrition_Rate_Percent
+FROM hr_employee_attrition
+GROUP BY Age_Group
+ORDER BY Age_Group;
---
+++ attrition.python.py
@@ -0,0 +1,50 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+
+df = pd.read_csv("WA_Fn-UseC_-HR-Employee-Attrition.csv")
+
+print(df.head())
+print(df.shape)
+print(df.info())
+print(df.isnull().sum())
+print(df.duplicated().sum())
+print(df.describe())
+
+sns.countplot(x="Attrition", data=df)
+plt.show()
+
+attrition_count = df["Attrition"].value_counts()
+print(attrition_count)
+
+attrition_percentage = df["Attrition"].value_counts(normalize=True) * 100
+print(attrition_percentage)
+
+plt.figure(figsize=(8,5))
+sns.countplot(x="Department", hue="Attrition", data=df)
+plt.title("Department-wise Attrition")
+plt.xticks(rotation=10)
+plt.show()
+
+plt.figure(figsize=(8,5))
+sns.histplot(df["Age"], bins=15, kde=True)
+plt.title("Age Distribution of Employees")
+plt.xlabel("Age")
+plt.ylabel("Number of Employees")
+plt.show()
+
+plt.figure(figsize=(8,5))
+sns.boxplot(x="Attrition", y="Age", data=df)
+plt.title("Age vs Attrition")
+plt.show()
+
+plt.figure(figsize=(12,8))
+numeric_df = df.select_dtypes(include=['int64', 'float64'])
+sns.heatmap(numeric_df.corr(), cmap="coolwarm")
+plt.title("Correlation Heatmap")
+plt.show()
